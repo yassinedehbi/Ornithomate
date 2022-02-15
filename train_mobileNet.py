2d111041ecb40dec7d9794fba41f11eb5b3a4462
yassinedehbi <yassinedehbi4@gmail.com>
2022-02-15 18:11:44 +0100

    3iw

--- train_mobileNet.py
+++ train_mobileNet.py
@@ -62,7 +62,7 @@ def _main(MY_PATH=MYY_PATH):
             # use custom yolo_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred})
 
-        batch_size = 16
+        batch_size = 32
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         #model.fit_generator(data_generator_wrapper(t_lines, batch_size, input_shape, anchors, num_classes),
         #        steps_per_epoch=max(1, num_train//batch_size),
@@ -75,14 +75,15 @@ def _main(MY_PATH=MYY_PATH):
                 steps_per_epoch=max(1, num_train//batch_size),
                 validation_data=data_generator_wrapper(v_lines, batch_size, input_shape, anchors, num_classes),
                 validation_steps=max(1, num_val//batch_size),
-                epochs=30,
+                epochs=60,
                 initial_epoch=0,
                 callbacks=[logging, checkpoint])
         model.save_weights(log_dir + 'trained_weights_stage_1.h5')
 
     # Unfreeze and continue training, to fine-tune.
     # Train longer if the result is not good.
-    if True:
+    i = 0
+    if i == 1:
         print("Unfreeze and continue training, to fine-tune.")
         for i in range(len(model.layers)):
             model.layers[i].trainable= True
